690cf5884b4a8b126dd97a4a03d2b095e769c4b9
Onder B <yazilimdonanim35@hotmail.com>
2022-09-18 14:49:28 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekle(Customer customer)
+        {
+            Console.WriteLine("Tebikler! " + customer.Adi + " Müşteri eklendi");
+        }
+
+        public void Listele(Customer customer)
+        {
+            Console.WriteLine("Tebikler! " + customer.Adi + " Müşteri listelendi");
+        }
+
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine("Tebikler! " + customer.Adi + " Müşteri silindi");
+        }
+
+        public void Duzenle(Customer customer)
+        {
+            Console.WriteLine("Tebikler! " + customer.Adi + " Müşteri düzenlendi");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Adi = "Ahmet";
+            customer1.Soyadi = "AYLAK";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Adi = "Behcet";
+            customer2.Soyadi = "BAKIR";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Adi = "Cevdet";
+            customer3.Soyadi = "CEYLAN";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Adi = "Davut";
+            customer4.Soyadi = "DEMİR";
+
+            Customer[] customers = new Customer[] {customer1, customer2, customer3, customer4 };
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + " " + customer.Adi + " " + customer.Soyadi);
+            }
+            Console.WriteLine("foreach döngüsü Listeleme Bitti");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("-----------Metotlar------------");
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(customer1);
+            Console.WriteLine(" ");
+            customerManager.Duzenle(customer2);
+            Console.WriteLine(" ");
+            customerManager.Listele(customer3);
+            Console.WriteLine(" ");
+            customerManager.Sil(customer4);
+
+
+        }
+    }
+}
